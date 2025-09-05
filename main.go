3f72964df6912e03765d778e68ce071c6cde113d
Nurlan Purkhanov <ppurkhanov@gmail.com>
2025-09-05 11:49:51 +0500

    created setWebAppUrl logic

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
@@ -11,6 +13,7 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"regexp"
 	"strings"
 	"syscall"
 	"time"
@@ -36,7 +39,7 @@ func getFlags() (token, port string) {
 }
 
 func main() {
-	_, port := getFlags()
+	token, port := getFlags()
 
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
@@ -65,7 +68,7 @@ func main() {
 		scanner := bufio.NewScanner(stdout)
 		for scanner.Scan() {
 			line := scanner.Text()
-			fmt.Println("LT:", line)
+			setWebAppUrl(token, getUrl(line))
 
 			if checkConnection(line) {
 				password, err := getPassword()
@@ -86,6 +89,17 @@ func main() {
 
 func checkConnection(scannedText string) bool {
 	return strings.Contains(scannedText, "your url is: https://")
+}
+
+func getUrl(line string) string {
+	re := regexp.MustCompile(`https://\S+`)
+	match := re.FindString(line)
+
+	if match == "" {
+		log.Fatal("can not found URL: ", match)
+	}
+
+	return match
 }
 
 func getPassword() (string, error) {
@@ -121,6 +135,46 @@ func getPassword() (string, error) {
 	return "", fmt.Errorf("после 3 попыток не удалось получить пароль: %w", lastErr)
 }
 
-// func setWebAppUrl(token, url string) error {
-// 	apiUrl := fmt.Sprintf("https://api.telegram.org/bot%s/setWebAppUrl", token)
-// }
+func setWebAppUrl(token, url string) error {
+	apiUrl := fmt.Sprintf("https://api.telegram.org/bot%s/setChatMenuButton", token)
+	params := map[string]map[string]any{
+		"menu_button": {
+			"type": "web_app",
+			"text": "Open App",
+			"web_app": map[string]string{
+				"url": url,
+			},
+		},
+	}
+
+	jsonData, err := json.Marshal(params)
+	if err != nil {
+		return fmt.Errorf("cannot marshal: %w", err)
+	}
+
+	resp, err := http.Post(apiUrl, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return fmt.Errorf("cannot send request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("cannot read response body: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("status code: %d, body: %s", resp.StatusCode, string(respBody))
+	}
+
+	var result map[string]any
+	if err := json.Unmarshal(respBody, &result); err != nil {
+		return fmt.Errorf("cannot unmarshal response: %w", err)
+	}
+
+	if ok, _ := result["ok"].(bool); !ok {
+		return fmt.Errorf("response not ok: %s", string(respBody))
+	}
+
+	return nil
+}
